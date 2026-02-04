55f26ef484fc2c7b474704170d2301c294ad6e27
Christian Raphael <christianraphael.1309@gmail.com>
2026-02-03 21:47:22 -0300

    wip add react use state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,33 @@
+import React from 'react'
+
 export default function Main() {
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => {
+        return (<li key={ingredient}>{ingredient}</li>)
+    })
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients =>  [...prevIngredients, newIngredient])
+    }
+    
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
