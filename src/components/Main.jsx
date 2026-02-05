a48eb8fbdf96b8e4155fcfa8084d90eed6804b3d
Christian Raphael <christianraphael.1309@gmail.com>
2026-02-04 22:54:08 -0300

    add get recipe container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,21 +3,19 @@ import React from 'react'
 export default function Main() {
     const [ingredients, setIngredients] = React.useState([])
 
-    const ingredientsListItems = ingredients.map(ingredient => {
-        return (<li key={ingredient}>{ingredient}</li>)
-    })
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-        setIngredients(prevIngredients =>  [...prevIngredients, newIngredient])
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    
+
     return (
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
-                <input 
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
@@ -25,9 +23,23 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {
+                ingredients.length > 0 && 
+                <section>
+                    <h2>Ingredients on hand:</h2>
+                    <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                    {
+                        ingredients.length > 3 && 
+                        <div className="get-recipe-container">
+                            <div>
+                                <h3>Ready for a recipe?</h3>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <button>Get a recipe</button>
+                        </div>
+                    }
+                </section>
+            }
         </main>
     )
 }
